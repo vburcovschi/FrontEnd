a98b128c2315e0657ea6b1905d9098af35baba9b
vburcovschi <68415281+vburcovschi@users.noreply.github.com>
2024-02-23 08:32:56 +0200

    Promise

---
+++ JS/AJAX/Promise/index.js
@@ -0,0 +1,24 @@
+// Флаг вызова resolve или reject. Можно переключать
+// для изменения результата
+let flag = true;
+
+// Создаем promise
+const myPromise = new Promise((resolve, reject) => {
+    if (flag) {
+        resolve("Успешное выполнение promise");
+    } else {
+        reject("Неуспешное выполнение promise");
+    }
+});
+
+// Выполняем promise
+myPromise
+    .then((result) => {
+        console.log('Обрабатываем resolve', result);
+    })
+    .catch((error) => {
+        console.log('Обрабатываем reject', error);
+    })
+    .finally(() => {
+        console.log('Выполняется всегда');
+    });
