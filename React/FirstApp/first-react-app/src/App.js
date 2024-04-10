e0ebfa82538520f267ee0e260f3c59724ab326fa
vburcovschi <68415281+vburcovschi@users.noreply.github.com>
2024-04-10 16:27:44 +0300

    Commponent

--- React/FirstApp/first-react-app/src/App.js
+++ React/FirstApp/first-react-app/src/App.js
@@ -1,22 +1,7 @@
-import './App.css';
-import Image from "./Image";
+import './App.css'
+import Image from "./components/Image";
 import React from "react";
-
-class Button extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = { clicked:false }
-  }
-  render(){
-    const { clicked } = this.state;
-    if (clicked) {
-      return 'Clicked'
-    }
-    return (
-      <button onClick={() => this.setState({clicked:true})}>Click me</button>
-  )
-  }
-}
+import Button from "./components/Button";
 
 function App() {
   return (
